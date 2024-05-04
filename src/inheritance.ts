fd209c6f579b8d79ba103a303d184924b4a9ec24
Shariar Islam <shariarn85@gmail.com>
2024-05-05 00:07:34 +0600

    extends use of parent class in inheritance

--- src/inheritance.ts
+++ src/inheritance.ts
@@ -1,7 +1,5 @@
 {
-
-    
-  class Student {
+  /*  class Student {
     name: string;
     age: number;
     address: string;
@@ -19,8 +17,8 @@
 
   const student1 = new Student("Niaj", 24, "GEC");
   //   student1.getSleep(5);
-
-  class Teacher {
+ */
+  /* class Teacher {
     name: string;
     age: number;
     address: string;
@@ -47,15 +45,62 @@
     }
   }
 
-  const teacher1 = new Teacher("Shariar", 24, "GEC", "Web Instructor");
-    teacher1.takeClass(6);
+  const teacher = new Teacher("Shariar", 24, "GEC", "Web Instructor");
+    // teacher.takeClass(6);
     
 
+ */
 
+  class Parent {
+    name: string;
+    age: number;
+    address: string;
+    // designation: string;
 
+    constructor(
+      name: string,
+      age: number,
+      address: string
+      //   designation: string
+    ) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+      //   this.designation = designation;
+    }
 
+    getSleep(numberOfHours: number) {
+      console.log(`${this.name} should sleep at least ${numberOfHours} hours`);
+    }
+  }
 
+  class Student1 extends Parent {
+    constructor(name: string, age: number, address: string) {
+      super(name, age, address);
+    }
+  }
 
+  const student1 = new Student1("Niaj", 24, "GEC");
+  //   student1.getSleep(5);
 
+  class Teacher1 extends Parent {
+    designation: string;
 
+    constructor(
+      name: string,
+      age: number,
+      address: string,
+      designation: string
+    ) {
+      super(name, age, address);
+      this.designation = designation;
+    }
+
+    takeClass(numberOfClass: number) {
+      console.log(`${this.name} take ${numberOfClass} hours of classes`);
+    }
+  }
+
+  const teacher1 = new Teacher1("Shariar", 24, "GEC", "Web Instructor");
+  // teacher1.takeClass(6);
 }
