98a2f4dc8b490fe8fc05213978103a28dda8dad2
Shariar Islam <shariarn85@gmail.com>
2024-05-05 20:24:02 +0600

    initial stage of polymorphism

---
+++ src/polymorphism.ts
@@ -0,0 +1,31 @@
+{
+  class Person {
+    getSleep() {
+      console.log("i am sleeping 8 hours a day");
+    }
+  }
+
+  class Student extends Person {
+    getSleep() {
+      console.log("i am sleeping 7 hours a day");
+    }
+  }
+
+  class Developer extends Person {
+    getSleep() {
+      console.log("i am sleeping 6 hours a day");
+    }
+  }
+
+  const getSleepingHours = (param: Person) => {
+    param.getSleep();
+  };
+
+  const person1 = new Person();
+  const person2 = new Student();
+  const person3 = new Developer();
+
+  getSleepingHours(person1);
+  getSleepingHours(person2);
+  getSleepingHours(person3);
+}
