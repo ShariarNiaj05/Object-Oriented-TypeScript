50d5a0d28ba7f90237f4bcd155203974d0c75873
Shariar Islam <shariarn85@gmail.com>
2024-05-05 19:51:49 +0600

    uses of static

--- src/staticsInOOP.ts
+++ src/staticsInOOP.ts
@@ -1,21 +1,34 @@
 {
   // static
   class Counter {
-    count: number = 0;
-    increment() {
-      return (this.count = this.count + 1);
+    static count: number = 0;
+
+    static increment() {
+      return (Counter.count += 1);
     }
-    decrement() {
-      return (this.count = this.count - 1);
+
+    static decrement() {
+      return (Counter.count -= 1);
     }
   }
 
   // The following instances are allocating different different memory
+  /*
   const instance1 = new Counter();
   console.log(instance1.increment());
 
   const instance2 = new Counter();
-    console.log(instance2.decrement());
-    
-    
+  console.log(instance2.decrement());
+ */
+
+  // i want to address the same memory allocation for the above example =>> have to use static
+
+  const instance1 = new Counter();
+  //   console.log(instance1.increment());
+  //   console.log(instance1.increment());
+  console.log(Counter.increment());
+  console.log(Counter.increment());
+
+  const instance2 = new Counter();
+  console.log(Counter.decrement());
 }
