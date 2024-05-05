c490aef6246b0c2bf46834a896f7b0a06aa0cf33
Shariar Islam <shariarn85@gmail.com>
2024-05-05 18:26:42 +0600

    simple logic and property for access modifier

--- src/acessModifiers.ts
+++ src/acessModifiers.ts
@@ -1,4 +1,22 @@
 {
-    // Access Modifiers
+  // Access Modifiers
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    private balance: number;
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this.balance = balance;
+    }
 
+    addDeposit(amount: number) {
+      this.balance += amount;
+    }
+  }
+
+  const goribAccount = new BankAccount(111, "X", 50);
+  //   goribAccount.balance = 0; //Property 'balance' is private and only accessible within class 'BankAccount'.
+  const newMoney = goribAccount.addDeposit(50);
+  console.log(newMoney);
 }
