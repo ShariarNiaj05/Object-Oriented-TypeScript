553c6ea1de4c3c845a01d58e66d24f4f94cdaa74
Shariar Islam <shariarn85@gmail.com>
2024-05-05 18:58:43 +0600

    private, public and readonly property for access modifier

--- src/acessModifiers.ts
+++ src/acessModifiers.ts
@@ -3,20 +3,24 @@
   class BankAccount {
     public readonly id: number;
     public name: string;
-    private balance: number;
+    private _balance: number;
     constructor(id: number, name: string, balance: number) {
       this.id = id;
       this.name = name;
-      this.balance = balance;
+      this._balance = balance;
     }
 
     addDeposit(amount: number) {
-      this.balance += amount;
+      this._balance += amount;
+    }
+    getBalance(): string {
+      return `Totla Balance is ${this._balance}`;
     }
   }
 
   const goribAccount = new BankAccount(111, "X", 50);
   //   goribAccount.balance = 0; //Property 'balance' is private and only accessible within class 'BankAccount'.
-  const newMoney = goribAccount.addDeposit(50);
-  console.log(newMoney);
+  goribAccount.addDeposit(50);
+    const myBalace = goribAccount.getBalance();
+    console.log(myBalace);
 }
