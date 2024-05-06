e988aa08d87f24944e044c621830c4af7101655d
Shariar Islam <shariarn85@gmail.com>
2024-05-06 19:06:29 +0600

    simple interface before starting abstraction

--- src/abstractionInOOP.ts
+++ src/abstractionInOOP.ts
@@ -1,10 +1,20 @@
 {
-// 
+  //
+  // Abstaction
+  /****
+   * Abstraction can be use by using two type
+   * 1. interface
+   * 2.abstract
+   *
+   * */
+  interface Vehicle1 {
+    name: string;
+    model: number;
+  }
+  const vehicle1: Vehicle1 = {
+    name: "Toyota",
+    model: 2020,
+  };
 
-
-
-
-
-
-// 
+  //
 }
